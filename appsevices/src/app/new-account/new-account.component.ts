f14088479183cef5acdd2a51c9161aee1c843b81
saranya <saranya@tangledai.com>
2021-06-25 14:34:38 +0530

    logging services updated and used in the component

--- appsevices/src/app/new-account/new-account.component.ts
+++ appsevices/src/app/new-account/new-account.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit,Output,EventEmitter} from '@angular/core';
-
+import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers:[LoggingService]
 })
 export class NewAccountComponent implements OnInit {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-  constructor() { }
+  constructor(private loggingService:LoggingService) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +20,7 @@ export class NewAccountComponent implements OnInit {
       name: accountName,
       status: accountStatus
     });
+    this.loggingService.logStatusChange(accountStatus);
     console.log('A server status changed, new status: ' + accountStatus);
   }
 
