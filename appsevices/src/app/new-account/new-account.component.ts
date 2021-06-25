e3715f5c724b61fa1a4fccb3cf6aa899caf7a0eb
saranya <saranya@tangledai.com>
2021-06-25 15:13:45 +0530

    added account service

--- appsevices/src/app/new-account/new-account.component.ts
+++ appsevices/src/app/new-account/new-account.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit,Output,EventEmitter} from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../accounts.services';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]
+  providers:[LoggingService,AccountService]
 })
 export class NewAccountComponent implements OnInit {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-  constructor(private loggingService:LoggingService) { }
+  constructor(
+    private loggingService:LoggingService,
+    private accountService:AccountService
+    ) { }
 
   ngOnInit(): void {
   }
@@ -16,10 +20,11 @@ export class NewAccountComponent implements OnInit {
   
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountService.addAccount({name:accountName,status:accountStatus});
     this.loggingService.logStatusChange(accountStatus);
     //console.log('A server status changed, new status: ' + accountStatus);
   }
