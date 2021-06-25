febc2386155d9e08a66ffce6727484353593f95a
saranya <saranya@tangledai.com>
2021-06-25 11:52:35 +0530

    account comp updated

--- appsevices/src/app/account/account.component.ts
+++ appsevices/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -10,6 +10,15 @@ export class AccountComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+   @Input() account: {name: string, status: string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    console.log('A server status changed, new status: ' + status);
   }
 
 }
