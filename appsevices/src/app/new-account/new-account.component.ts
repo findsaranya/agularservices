5d1e74e4d1164e83a18d1c7f1287fc053508d9d2
saranya <saranya@tangledai.com>
2021-06-25 11:42:03 +0530

    new-account updated

--- appsevices/src/app/new-account/new-account.component.ts
+++ appsevices/src/app/new-account/new-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent implements OnInit {
-
+  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus
+    });
+    console.log('A server status changed, new status: ' + accountStatus);
+  }
+
 
 }
